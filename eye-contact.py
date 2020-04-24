a1818ce1ab4cbfbffef9140505ccfe92dc3e5f21
Denys Potapov <denys.potapov@gmail.com>
2020-04-24 14:43:02 +0300

    ready aparams

--- eye-contact.py
+++ eye-contact.py
@@ -20,10 +20,15 @@ def scale_box2d(box, scale):
     h, w = box[1]
     return (box[0], (h * scale, w * scale), box[2])
 
-# def to_polar(p1, p2):
-#     """get a length and angel"""
 
-#     dx = p2[0] p1[0]
+def to_polar(p1, p2):
+    """Get a length and angel in degrees."""
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+
+    return math.hypot(dx, dy), math.degrees(math.atan2(dy, dx))
+
+
 class EyeContact:
     """EyeContact."""
 
@@ -35,29 +40,33 @@ class EyeContact:
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(MODEL_PATH)
 
-        self.centers = []
+        centers = []
         elipses = []
         for eye in self.detect_eyes(img):
-            self.centers.append(eye.mean(axis=0))
+            centers.append(eye.mean(axis=0))
 
             ellipse = cv2.fitEllipse(eye)
             mask_ellipse = scale_box2d(ellipse, EYE_SCALE)
             elipses.append(mask_ellipse)
             cv2.ellipse(self.mask, mask_ellipse, 255, -1)
+
         self.mask = cv2.GaussianBlur(self.mask, (EYE_BLUR, EYE_BLUR), 0)
 
-        # crop everythong
-        points = list(cv2.boxPoints(elipses[0])) + list(cv2.boxPoints(elipses[1]))
-        (left, top, w, h) = cv2.boundingRect(np.array(points))
+        # crop everything
+        points = list(
+            cv2.boxPoints(elipses[0])) + list(cv2.boxPoints(elipses[1]))
+        (left, top, self.w, self.h) = cv2.boundingRect(np.array(points))
         left, top = left - EYE_BLUR, top - EYE_BLUR
-        w, h = w + EYE_BLUR * 2, h + EYE_BLUR * 2
+        self.w, self.h = self.w + EYE_BLUR * 2, self.h + EYE_BLUR * 2
         self.img = self.img[top:top + h, left: left + w]
         self.mask = self.mask[top:top + h, left: left + w]
 
-        self.centers = [(x - left, y - top) for (x, y) in self.centers]
-        print(self.centers)
-        # 
-        # ]
+        # calculate the rest of params
+        self.length, self.angle = to_polar(centers[0], centers[1])
+        self.center = (
+            (centers[1][0] + centers[0][0]) // 2,
+            (centers[1][1] + centers[0][0]) // 1
+        )
 
     def detect_eyes(self, img):
         """Detect eyes on image."""
@@ -78,12 +87,16 @@ class EyeContact:
 
     def open_eyes(self, img):
         """Replace eyes with opened."""
-        pass
+        
+        return img
+
 
 open_img = cv2.imread(sys.argv[1])
+test_img = cv2.imread(sys.argv[2])
 
 eye_contact = EyeContact(open_img)
 
 cv2.imshow("Output", eye_contact.img)
-cv2.imshow("mask", eye_contact.mask)
+cv2.imshow("Result", eye_contact.open_eyes(test_img))
+print(eye_contact.center)
 cv2.waitKey(10000)
